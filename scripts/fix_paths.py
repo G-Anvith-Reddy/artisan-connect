6fc9ff40dcb590469bcd72787eea168dbf3b048b
G-Anvith-Reddy <anvithreddy3322@gmail.com>
2025-09-21 22:50:09 +0530

    Update fix_paths.py

--- scripts/fix_paths.py
+++ scripts/fix_paths.py
@@ -1,1 +1,11 @@
 
+# scripts/fix_paths.py
+from pathlib import Path
+from backend.db import SessionLocal
+from backend.models import Product
+
+db = SessionLocal()
+for p in db.query(Product).all():
+    p.image_path = Path(p.image_path).name
+db.commit()
+db.close()
